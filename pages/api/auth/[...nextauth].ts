ce0c88d37bef26d53d5bb994cf269ff3c3cda320
VJS <minsung_96@naver.com>
2023-04-11 23:58:40 +0900

    Github & Google Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,21 @@ import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt"
 
-import prismadb from '../../lib/prismadb'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+import prismadb from '../../../lib/prismadb'
 
 export default NextAuth({
     providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID || '',
+            clientSecret: process.env.GOOGLE_SECRET || ''
+        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
